e220e0f46a4c738e192eb4e7483158b94479ca48
SeyranGit <barsegyan1806@gmail.com>
2024-11-10 16:21:08 +0300

    setting the -Wno-reserved-macro-identifier flag for clang

--- others/fa/build.bat
+++ others/fa/build.bat
@@ -1,3 +1,3 @@
 @echo off
-clang full_adder.c -o Release/full_adder.exe -Weverything
+clang full_adder.c -o Release/full_adder.exe -Weverything -Wno-reserved-macro-identifier
 Release\full_adder.exe
