cd0b036c8fc39fd9e4b2cca081f1f2e3d0130293
SeyranGit <barsegyan1806@gmail.com>
2024-12-09 00:49:37 +0300

    format

--- data-structures/stack/stack/stack.c
+++ data-structures/stack/stack/stack.c
@@ -14,8 +14,7 @@ static error_code push_impl(Stack *stack, int32 item) {
 static int32 pop_impl(Stack *stack) {
   uint32 last_index = stack->inner_list.len - 1;
   int32 result = stack->inner_list.get(
-    &(stack->inner_list),
-    last_index
+    &(stack->inner_list), last_index
   );
   stack->inner_list.remove(&(stack->inner_list), last_index);
   return result;
